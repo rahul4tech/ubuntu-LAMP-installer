d0937ae35c8d8b0b0ead56e31bc2e7de6330853d
Rahul Sinha <rahul@nextechinc.net>
2023-11-28 02:21:40 +0530

    fine tuned

--- ubuntu-LAMP-installer.sh
+++ ubuntu-LAMP-installer.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+
 # Ask which way to install, then install lamp stack or with /var/www/html rsync from another server
+
+# clear the screen
+clear
+
+# greeting message to user
+echo "Welcome to Ubuntu LAMP Installer"
+echo ""
+
+
 
 # Ask which way to install
 echo "Which way to install?"
@@ -6,12 +17,24 @@ echo "1. Install LAMP stack"
 echo "2. Install LAMP stack with /var/www/html rsync from another server"
 read -p "Enter your choice: " choice
 
+# check if $choice is empty or not integer
+if [ -z "$choice" ] || ! [[ "$choice" =~ ^[0-9]+$ ]]
+then
+    echo "Wrong choice, exiting..."
+    exit 1
+fi
+
+
 # check if choice is 1 or 2 else exit
 if [ $choice -eq 1 ] || [ $choice -eq 2 ]
 then
     
     # install LAMP stack
-    echo "Please Wait Installing Shortner Server Stack..."
+    echo ""
+    echo "Please Wait Installing Ubuntu LAMP Stack Server..."
+    echo "Remember: configure your firewall to allow http and https traffic"
+    echo "Remember: Will Not Configure MySQL, Please Configure MySQL Manually"
+    echo "Note: This will take some time, please be patient..."
     echo ""
     # Update the system and suppress all output
     sudo DEBIAN_FRONTEND=noninteractive apt-get -yq update > /dev/null 2>&1
@@ -23,7 +46,7 @@ then
     sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install lamp-server^ unzip php-curl rsync > /dev/null 2>&1
 
     # enable mod_rewrite
-    sudo a2enmod rewrite
+    sudo a2enmod rewrite > /dev/null 2>&1
 
     apache2_conf=$(cat <<EOF
 # CONFIGURED BY UBU-LAMP-INSTALLER
@@ -43,13 +66,15 @@ EOF
     then
 
         # mv /var/www/html to /var/www/html-orig
-        sudo mv /var/www/html /var/www/html-orig
+        sudo mv /var/www/html /var/www/html-orig > /dev/null 2>&1
         # ask for rsync server ip
         read -p "Enter rsync server ip: " rsync_server_ip
         rsync -avzh --progress root@$rsync_server_ip:/var/www/html /var/www/html
     fi
-    sudo systemctl restart apache2
+    sudo systemctl restart apache2 > /dev/null 2>&1
 
+    
+    echo ""
     echo "Installed, About To Reboot Server"
     echo ""
     sudo sleep 5
